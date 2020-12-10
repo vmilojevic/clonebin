e0034bb507e8587cc8fd372455aa96346a564209
vmilojevic <vladan.milojevic@elfak.rs>
2020-12-10 22:05:05 +0100

    Add project

---
+++ src/main/java/com/nsi/clonebin/ClonebinApplication.java
@@ -0,0 +1,13 @@
+package com.nsi.clonebin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonebinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClonebinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nsi/clonebin/ClonebinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsi.clonebin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonebinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
