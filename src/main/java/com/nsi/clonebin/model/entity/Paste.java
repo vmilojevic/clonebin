993ccd9f4e231a104366caa08086bb2b32b9f0c9
vladimir.nesic <vladimir.nesic@dualsoft.net>
2021-01-14 20:10:52 +0100

    dodate nove tabele i fragmenti

--- src/main/java/com/nsi/clonebin/model/entity/Paste.java
+++ src/main/java/com/nsi/clonebin/model/entity/Paste.java
@@ -1,8 +1,50 @@
 package com.nsi.clonebin.model.entity;
 
-//@Entity
-//@Table(name = "paste")
-//@Data
+import java.sql.Timestamp;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "paste")
+@Data
 public class Paste {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "user_id")
+    private UUID userId;
+    
+    @Column(name = "folder_id")
+    private UUID folderId;
+
+    @Column(name = "title")
+    private String title;
+    
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp cratedAt;
+    
+    @Column(name = "expires_at")
+    private Timestamp expiresAt;
+
+    public Paste(UUID userId, UUID folderId, String title, String content, Timestamp cratedAt, Timestamp expiresAt) {
+        this.userId = userId;
+        this.folderId = folderId;
+        this.title = title;
+        this.content = content;
+        this.cratedAt = cratedAt;
+        this.expiresAt = expiresAt;
+    }
+    
 }
